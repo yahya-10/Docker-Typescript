a8b2d4c5106a53c94cd8fe56598be4bf525e9481
Yahya Akermi <71073320+yahya-10@users.noreply.github.com>
2026-05-17 21:38:46 +0000

    Feat: Add centralized error handler'

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,23 @@
-import express from 'express'
-import dotenv from 'dotenv'
+import express from "express";
+import dotenv from "dotenv";
 
-import { pool } from './config/database.ts'
+import { pool } from "./config/database.ts";
+import { asyncHandler, errorHandler } from "./shared/ErrorRequestHandler.ts";
 
-const app = express()
+const app = express();
 
-dotenv.config()
-app.use(express.json())
+dotenv.config();
+app.use(express.json());
+app.use(errorHandler);
 
-app.get('/', async (req, res) => {
-  try {
-    const result = await pool.query('SELECT * FROM users');
+app.get(
+  "/",
+  asyncHandler(async (req, res) => {
+    const result = await pool.query("SELECT * FROM users");
     res.json(result.rows);
-  } catch (error) {
-    res.status(500).json({ error: 'Database query failed' });
-  }
-});
+  }),
+);
 
-app.listen(process.env.PORT, () =>  {
-    console.log(`App is running on port ${process.env.PORT}`)
-})
+app.listen(process.env.PORT, () => {
+  console.log(`App is running on port ${process.env.PORT}`);
+});
---
+++ src/shared/ErrorRequestHandler.ts
@@ -0,0 +1,41 @@
+import type { Request, Response, NextFunction, RequestHandler } from "express";
+
+export class AppError extends Error {
+  statusCode: number;
+  isOperational: boolean;
+
+  constructor(message: string, statusCode: 500) {
+    super(message);
+    this.statusCode = statusCode;
+    this.isOperational = true;
+
+    Error.captureStackTrace(this, this.constructor);
+  }
+}
+
+export const errorHandler = (
+  err: Error | AppError,
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  console.error(`Custom dev error: ${err}`);
+
+  if (err instanceof AppError) {
+    return res.status(err.statusCode).json({
+      success: false,
+      message: err.message,
+    });
+  }
+
+  return res.json({
+    success: false,
+    message: "Internal Error",
+  });
+};
+
+export const asyncHandler =
+  (fn: RequestHandler): RequestHandler =>
+  (req: Request, res: Response, next: NextFunction) => {
+    Promise.resolve(fn(req, res, next)).catch(next);
+  };
